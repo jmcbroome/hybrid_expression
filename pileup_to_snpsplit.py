aedd1e5f90fbfef7e5f8f288696fc488f867bf7d
Jakob <jmcbroome@gmail.com>
2020-10-12 10:42:21 -0700

    update to filter multiple-mapped reference sites

--- pileup_to_snpsplit.py
+++ pileup_to_snpsplit.py
@@ -9,12 +9,14 @@ for entry in sys.stdin:
     if entry[0] == '#':
         continue
     spent = entry.strip().split()
+    if spent[3] != '1':
+        continue #ignore regions with multiple representations in the alternative
     id = 'asm_aln'
     chr = spent[0]
     pos = spent[1]
     ref = spent[2]
     val = spent[5]
-    if spent[4] in 'ACGT': #should be one of these.
+    if spent[4] in 'ACGT': #should be one of these. Only keep areas with depth 1 for simplicity (1-to-1 correspondence)
         alt = spent[4]
         refsnp = ref + '/' + alt
         print("\t".join([id, chr, pos, val, refsnp]))
